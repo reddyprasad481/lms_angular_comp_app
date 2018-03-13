784c05b5f213740f5196e82baad259162f82f1cc
reddyprasad481 <reddyprasad481@gmail.com>
2018-03-14 00:55:34 +0530

    pdf form submit example

--- app/scripts/footer/footer.component.js
+++ app/scripts/footer/footer.component.js
@@ -6,13 +6,33 @@ angular.module('footer')
 
 	function FooterController(appService) {
 		//var self = this;
-
-       /* appService.make({endpoint:'customers/getCustomers',method:'GET', data:{}})
+        var $ctrl = this;
+        $ctrl.pdfData =null;
+        appService.make({endpoint:'customers/getCustomers',method:'GET', data:{}})
 			.then(function(response){
 				console.log(response);
+             $ctrl.pdfData =  response; 
+            console.log($ctrl.pdfData);
+          //  var pdfUrl = URL.createObjectURL(response);
+          //  var printwWindow = $window.open(pdfUrl);
+           // printwWindow.print();
+            
 			},function(error){
 				console.log(error);
-			});*/
+			});
+
+          this.printPdf = function() {
+            
+              var pdfForm = document.getElementById('pdfForm');
+              
+              pdfForm.action ='http://localhost:8080/spring_rest_services/service/customers/getpdfForInput';
+                  
+             pdfForm.method='POST';
+              //pdfForm.body =99;//$ctrl.pdfData.data;
+              console.log($ctrl.pdfData.data);
+              pdfForm.submit();
+
+          };
 	}
 
 	FooterController.$inject = ['appService'];
